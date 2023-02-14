f6d02a53a73903d2152744988711f2ff92a847cb
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-02-14 15:30:21 -0600

    Login CHecks wether a user is on the database, then checks for password before proceesing. Failure will be answered with a 401.html page stating that either username of password are incorrect.

--- controllers/index.js
+++ controllers/index.js
@@ -7,5 +7,16 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use('/login', login);
-
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the user to the login page
+    // This is directly from the `/gallery/:id` and `/painting/:id` routes
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+      // If the user is logged in, execute the route function that will allow them to view the gallery
+      // We call next() if the user is authenticated
+      next();
+    }
+  };
+router.use('/html/about.html', withAuth);
 module.exports = router;
--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,33 @@
 const router = require('express').Router();
+const {User} = require('../models');
 
-
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     //Check if valid password
+    console.log(req.body);
+    try {
+        const currentUser = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if (!currentUser) {
+            res.status(401).redirect('../html/401.html');
+            return;
+        }
+        const currentUserPassword = await currentUser.checkPassword(req.body.password);
+
+        if (!currentUserPassword) {
+            res.status(401).redirect('../html/401.html');
+            return;
+        }
+    } catch (err) {
+        res.redirect('/');
+        return
+    }
     //log into session
+    req.session.save(() => {
+        req.session.loggedIn = true;
+      });
     //redirect to about 
     res.redirect('/html/about.html');
 })
