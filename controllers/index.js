dc01233af2cf177d55c5b2cb23f44a876f7edc82
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-02-19 14:38:41 -0600

    Trivia Handlebar No can read questions

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const login = require('./login.js');
 const apiRoutes = require('./api');
 const withAuth = require('../utils/auth.js')
-//const trivia = require('./trivia.js');
+const trivia = require('./trivia.js');
 
 
 router.use('/api', apiRoutes);
@@ -11,5 +11,5 @@ router.use('/login', login);
 router.use('/about',withAuth, (req,res) => {
     res.redirect('/html/about.html')
 } );
-//router.use('/trivia', trivia);
+router.use('/trivia', trivia);
 module.exports = router;
--- controllers/trivia.js
+++ controllers/trivia.js
@@ -1,45 +1,10 @@
 const router = require('express').Router();
-const {Question} = require('../models');
-
+const {Question} = require('../models/Question.js');
+const questions = require('../seeds/questions.json');
 router.get('/', async (req, res) => {
     //Get all of the questions  (These were prepopulated in the schema.sql file since they are read only)
     try {
-        // const energyData = await Question.findAll({
-        //     where: {
-        //         category: 1
-        //     }
-        // });
-        // const attachmentData = await Question.findAll({
-        //     where: {
-        //         category: 2
-        //     }
-        // });
-        // const moralData = await Question.findAll({
-        //     where: {
-        //         category: 3
-        //     }
-        // });
-        // const haveyouData = await Question.findAll({
-        //     where: {
-        //         category: 4
-        //     }
-        // });
-        // const lovelangData = await Question.findAll({
-        //     where: {
-        //         category: 5
-        //     }
-        // });
-        // const energy = energyData.map(e => e.get({plain: true}));
-        // const attachment = attachmentData.map(e => e.get({plain: true}));
-        // const moral = moralData.map(e => e.get({plain: true}));
-        // const haveyou = haveyouData.map(e => e.get({plain: true}));
-        // const lovelang = lovelangData.map(e => e.get({plain: true}));
-        const questionsData = await Question.findAll();
-        console.log('\n\nInside\n\n');
-        console.log(questionsData);
-        const questions = await questionsData.map(e => e.get({plain: true}));
-        console.log(questions);
-        //res.render('trivia', {energy, layout: false});
+        
         res.render('trivia', {questions, layout: false});
     } catch (err) {
         render('error', {err});
@@ -50,22 +15,10 @@ router.get('/', async (req, res) => {
 
 module.exports = router;
 
-/*
-ry {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-
-*/
+        // const questionsData = await Question.findAll();
+        // console.log('\n\nInside\n\n');
+        // console.log(questionsData);
+        // const questions = await questionsData.map(e => e.get({plain: true}));
+        // console.log(questions);
+        // //res.render('trivia', {energy, layout: false});
+        // res.render('trivia', {questions, layout: false});
