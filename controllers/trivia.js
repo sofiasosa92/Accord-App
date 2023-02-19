06912bf58737f591641034090d52edef080c4e03
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-02-19 14:41:18 -0600

    Heroku Test This commit is used to test heroku

--- controllers/trivia.js
+++ controllers/trivia.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const {Question} = require('../models/Question.js');
 const questions = require('../seeds/questions.json');
+
 router.get('/', async (req, res) => {
     //Get all of the questions  (These were prepopulated in the schema.sql file since they are read only)
     try {
@@ -14,11 +14,3 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router;
-
-        // const questionsData = await Question.findAll();
-        // console.log('\n\nInside\n\n');
-        // console.log(questionsData);
-        // const questions = await questionsData.map(e => e.get({plain: true}));
-        // console.log(questions);
-        // //res.render('trivia', {energy, layout: false});
-        // res.render('trivia', {questions, layout: false});
