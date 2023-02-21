07e65c71cf658ef0288cf61618147d4961070527
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-02-20 21:03:59 -0600

    Changed the sync force to true

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
 .then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
